fe5796cc789826c0cee1fb07ec4d53c301413667
spewite <jiz.j12.a4@gmail.com>
2025-03-02 21:32:48 +0100

    Remove logs so i can see the logs with docker logs.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,8 +4,8 @@ worker_class = "sync"
 loglevel = "info"
 
 # Logs de acceso y error
-accesslog = "/var/log/gunicorn/access.log"
-errorlog = "/var/log/gunicorn/error.log"
+# accesslog = "/var/log/gunicorn/access.log"
+# errorlog = "/var/log/gunicorn/error.log"
 
 # Formato del log de acceso
 access_log_format = "%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
