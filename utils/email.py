8d235c6588438e92e021b22a62ee5407ae3391d0
spewite <jiz.j12.a4@gmail.com>
2025-03-31 20:02:04 +0200

    Add file attachment to email notifications

--- utils/email.py
+++ utils/email.py
@@ -1,12 +1,13 @@
 import os
+import mimetypes
 from email.message import EmailMessage
 import smtplib
 
 from flask import current_app
 
-def send_email_notification(subject, body):
+def send_email_notification(subject, body, attachment_path=None):
     """
-    Sends an email using SMTP.
+    Sends an email using SMTP. Optionally attaches a file if attachment_path is provided.
     """
     
     EMAIL_ADDRESS = os.getenv('EMAIL_USER')
@@ -18,6 +19,19 @@ def send_email_notification(subject, body):
     msg['From'] = EMAIL_ADDRESS
     msg['To'] = PERSONAL_EMAIL
     msg.set_content(body)
+    
+    if attachment_path:
+        try:
+            with open(attachment_path, 'rb') as f:
+                file_data = f.read()
+            file_name = os.path.basename(attachment_path)
+            mime_type, _ = mimetypes.guess_type(attachment_path)
+            if mime_type is None:
+                mime_type = 'application/octet-stream'
+            maintype, subtype = mime_type.split('/', 1)
+            msg.add_attachment(file_data, maintype=maintype, subtype=subtype, filename=file_name)
+        except Exception as attach_err:
+            current_app.logger.error(f"Failed to attach file: {attach_err}")
     
     try:
         # Using Gmail's SMTP server as an example
